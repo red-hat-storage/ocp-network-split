e130fedab22d1106a71b96e8480183471b372d5b
Martin Bukatovič <mbukatov@redhat.com>
2023-01-16 21:06:49 +0100

    bump year

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'ocp-network-split'
-copyright = '2021, Martin Bukatovič'
+copyright = '2023, Martin Bukatovič'
 author = 'Martin Bukatovič'
 
 
