d1d0dc2dc0694f7b2858a96c3367141deddd0228
Martin Bukatovič <mbukatov@redhat.com>
2023-01-17 21:19:10 +0100

    network-latency.sh [-l LATSPEC] option
    Implementing support for zone specific latencies.
    
    Example:
    
      $ ./network-latency.sh -d -l AB=25 -l AC=35 5
    
    This will set:
    
    - 25ms (50ms RTT) latency between zones A and B
    - 35ms (70ms RTT) latency between zones A and C
    -  5ms (10ms RTT) latency otherwise (connection between B and C is left)
    
    Addressing https://gitlab.com/mbukatov/ocp-network-split/-/issues/9

--- ocpnetsplit/network-latency.sh
+++ ocpnetsplit/network-latency.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021 Martin Bukatovič <mbukatov@redhat.com>
+# Copyright 2023 Martin Bukatovič <mbukatov@redhat.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,17 @@
 show_help()
 {
   echo "Configure egress network latency via netem qdisc for a 3 zone cluster"
-  echo "Usage: $(basename "${0}") [-d] <egress latency>"
+  echo
+  echo "Usage: $(basename "${0}") [-d] [-l LATSPEC] <default egress latency>"
+  echo
+  echo "Where 'LATSPEC' defines specific latency between particular zones."
+  echo "Eg.: 'AC=20' will set 20ms latency between zones A and C, while the"
+  echo "rest of inter zone connections will use the default latency."
+  echo
+  echo The default latency is mandatory, while the optional zone specific one
+  echo can be specified multiple times, for each zone connection as necessary.
+  echo
+  echo "Examples: $(basename "${0}") -l AB=25 -l AC=25 5"
 }
 
 if [[ $# = 0 ]]; then
@@ -25,21 +35,48 @@ if [[ $# = 0 ]]; then
   exit
 fi
 
-# debug mode
-if [[ $1 = "-d" ]]; then
+# make sure we don't reuse variables from the outside environment by mistake
+unset DEBUG_MODE
+
+# dict for specific latencies
+declare -A latspec
+
+while getopts "dl:h" OPT; do
   # shellcheck disable=SC2209
-  DEBUG_MODE=echo
-  shift
-else
-  unset DEBUG_MODE
+  case $OPT in
+  d) DEBUG_MODE=echo;;
+  l) if [[ "$OPTARG" =~ ^([ABCX]{2})=([0-9]+)$ ]]; then
+       zones=$(echo "${BASH_REMATCH[1]}" | grep -o . | sort | tr -d "\n");
+       value=${BASH_REMATCH[2]};
+       if [[ -n ${latspec[$zones]} ]]; then
+         echo "Specific latency for $zones is defined multiple times." >&2;
+         exit 1
+       fi
+       latspec[$zones]=$value;
+     else
+       echo "Invalid specific latency specified: ${OPTARG}" >&2;
+       exit 1;
+     fi;;
+  h) show_help; exit;;
+  *) show_help; exit 1;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+if [[ $# = 0 ]]; then
+  echo "The default egress latency not specified!" >&2
+  exit 1
 fi
 
-# integer with egress latency is the only argument of the script
-case $1 in
-  help|-h)   show_help; exit;;
-  [0-9]*)    latency=$1; shift;;
-  *)         show_help; exit 1
-esac
+# integer with default egress latency is the only mandatory argument of the
+# script
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  latency=${1}
+else
+  echo "The default egress latency specified $1 is not an integer value." >&2
+  exit 1
+fi
 
 # check zone configuration and detect current zone (we are running inside)
 script_dir=$(realpath "$(dirname "$0")")
@@ -53,12 +90,42 @@ echo "current zone: $current_zone"
 
 # locate main network interface (assuming all nodes are on a single network)
 iface=$(ip route show default | cut -d' ' -f5)
+echo "network interface: $iface"
 
-# TODO: polish this to create as few changes in traffic queues as possible,
-# so that the original configuration could be restored without node reboot
+# delete all current qdiscss
+# TODO: polish this so that the original configuration could be restored
+# TODO: instead of deleting the original qdiscs, just alter it (would be
+# more complex and error prone, it's not clear it's worth the effort)
 $DEBUG_MODE tc qdisc del dev "${iface}" root
-$DEBUG_MODE tc qdisc add dev "${iface}" root handle 1: prio
-$DEBUG_MODE tc qdisc add dev "${iface}" parent 1:1 handle 2: netem delay "${latency}"ms
+
+# dict for tracking qdisc with specific latency
+declare -A qdisc_handles
+# there will always be a qdisc with handle 1:4 for the default latency, so the
+# next free minor handle is 1:5, hence:
+next_minor_num=5
+# for each zone specific delay value, allocate qdisc handle in qdisc_handles
+for ZONES in "${!latspec[@]}"; do
+  spec_latency=${latspec[$ZONES]}
+  if [[ $latency -eq $spec_latency ]]; then
+    continue
+  fi
+  if [[ -z ${qdisc_handles[${spec_latency}]} ]]; then
+    qdisc_handles[${spec_latency}]=${next_minor_num}
+    ((next_minor_num++))
+  fi
+done
+# check how many bands we need: we won't touch the 3 default bands, so we
+# will need one band for each lantecy
+band_num=$((next_minor_num-1))
+
+# define new qdisc structure
+$DEBUG_MODE tc qdisc add dev "${iface}" root handle 1: prio bands "${band_num}"
+$DEBUG_MODE tc qdisc add dev "${iface}" parent 1:4 handle 40: netem delay "${latency}"ms
+# for each zone specific delay value, define new qdisc with given latency
+for SP_LAT in "${!qdisc_handles[@]}"; do
+  minor_num=${qdisc_handles[$SP_LAT]}
+  $DEBUG_MODE tc qdisc add dev "${iface}" parent 1:${minor_num} handle ${minor_num}0: netem delay "${SP_LAT}"ms
+done
 
 # create tc filter/classifier for nodes in other zones, and direct traffic
 # heading to them via netem qdisc
@@ -67,6 +134,23 @@ for zone_name in ZONE_A ZONE_B ZONE_C; do
     continue
   fi
   for ip_addr in ${!zone_name}; do
-    $DEBUG_MODE tc filter add dev "${iface}" parent 1: protocol ip prio 2 u32 match ip dst ${ip_addr}/32 flowid 3:1
+    # check if there is a specific latency between current_zone and zone_name,
+    # reling on lex. ordering of zone letters in latspec asoc. array keys
+    ZX=${current_zone#ZONE_}
+    ZY=${zone_name#ZONE_}
+    if [[ ${ZX} < ${ZY} ]]; then
+      spec_latency=${latspec[${ZX}${ZY}]}
+    else
+      spec_latency=${latspec[${ZY}${ZX}]}
+    fi
+    # if a specific latency is defined, use qdiscs for this latency, otherwise
+    # use the default
+    if [[ -z $spec_latency ]]; then
+      handle=1:4
+    else
+      handle=1:${qdisc_handles[$spec_latency]:-4}
+    fi
+    # finally create a classifier
+    $DEBUG_MODE tc filter add dev "${iface}" parent 1: protocol ip prio 1 u32 match ip dst ${ip_addr}/32 flowid ${handle}
   done
 done
